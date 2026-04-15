391d8e42f91437c17c0e96381c66ca8cb3536d09
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-04-15 21:49:27 +0300

    add pojo model

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -2,12 +2,35 @@ package tests;
 
 import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
+import models.pojo.RegistrationBodyPojoModel;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class RegistrationTests {
+
+    @Test
+    public void successfulRegistrationTest_BadPractice() {
+
+        Faker faker = new Faker();
+        String userName = faker.internet().domainWord();
+        String password = faker.internet().password(4, 6);
+
+        String data = "{\"username\": \"" + userName + "\",\"password\":\"" + password + "\"}";
+
+        given()
+                .body(data)
+                .contentType(ContentType.JSON)
+                .when()
+                .log().all()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("username", is(userName))
+                .body("id", notNullValue());
+    }
 
     @Test
     public void successfulRegistrationTest() {
@@ -29,6 +52,63 @@ public class RegistrationTests {
                 .statusCode(201)
                 .body("username", is(userName))
                 .body("id", notNullValue());
+    }
+
+    @Test
+    public void successfulRegistrationTest_withPojo() {
+
+        Faker faker = new Faker();
+        String userName = faker.internet().domainWord();
+        String password = faker.internet().password(4, 6);
+
+        RegistrationBodyPojoModel data = new RegistrationBodyPojoModel();
+        data.setUsername(userName);
+        data.setPassword(password);
+
+        given()
+                .body(data)
+                .contentType(ContentType.JSON)
+                .when()
+                .log().all()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("username", is(userName))
+                .body("id", notNullValue());
+    }
+
+    @Test
+    public void existingUserTest() {
+
+        Faker faker = new Faker();
+        String userName = faker.internet().domainWord();
+        String password = faker.internet().password(4, 6);
+
+        String data = "{\"username\": \"" + userName + "\",\"password\":\"" + password + "\"}";
+
+        given()
+                .body(data)
+                .contentType(ContentType.JSON)
+                .when()
+                .log().all()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("username", is(userName))
+                .body("id", notNullValue());
+
+        given()
+                .body(data)
+                .contentType(ContentType.JSON)
+                .when()
+                .log().all()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("username[0]", is("A user with that username already exists."));
     }
 
     @Test
@@ -97,7 +177,7 @@ public class RegistrationTests {
     }
 
     @Test
-    public void unvalidUserName() {
+    public void invalidUserName() {
 
         Faker faker = new Faker();
         String userName = "Maxwell Kilback";
@@ -117,7 +197,4 @@ public class RegistrationTests {
                 .body("username", is(userName))
                 .body("id", notNullValue());
     }
-
 }
-
-
