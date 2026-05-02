a206ae54676a72445a05e138660ebffc71267c6e
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-05-02 14:21:42 +0300

    Add login test

--- src/test/java/tests/examples/LoginTestsWithoutSpecs.java
+++ src/test/java/tests/examples/LoginTestsWithoutSpecs.java
@@ -1,31 +1,44 @@
-package tests;
+package tests.examples;
 
+import io.restassured.http.ContentType;
 import models.login.LoginBodyModel;
 import models.login.LoginSuccessfulResponseModel;
 import models.login.LoginUnsuccessfulResponseModel;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import tests.TestBase;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static specs.login.LoginSpec.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.notNullValue;
 
-public class LoginTests extends TestBase {
+public class LoginTestsWithoutSpecs extends TestBase {
 
     String userName = "Agzamurai";
     String password = "Qwer1234";
     String wrongPassword = "Qwer4321";
 
     @Test
+    @Disabled
     public void successfulLoginTest() {
 
         LoginBodyModel loginData = new LoginBodyModel(userName, password);
 
-        LoginSuccessfulResponseModel loginRsponse = given(loginRequestSpec)
+        LoginSuccessfulResponseModel loginRsponse = given()
                 .body(loginData)
+                .contentType(ContentType.JSON)
                 .when()
+                .log().all()
+                .basePath("/api/v1")
                 .post("/auth/token/")
                 .then()
-                .spec(successfulLoginSpec)
+                .log().all()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/login/login_response_schema.json"))
+                .body("refresh", notNullValue())
+                .body("access", notNullValue())
                 .extract().as(LoginSuccessfulResponseModel.class);
 
         String expectedTokenPath = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
@@ -38,16 +51,23 @@ public class LoginTests extends TestBase {
     }
 
     @Test
+    @Disabled
     public void wrongPassword() {
 
         LoginBodyModel loginData = new LoginBodyModel(userName, wrongPassword);
 
-        LoginUnsuccessfulResponseModel loginResponse = given(loginRequestSpec)
+        LoginUnsuccessfulResponseModel loginResponse = given()
                 .body(loginData)
+                .contentType(ContentType.JSON)
                 .when()
+                .log().all()
+                .basePath("/api/v1")
                 .post("/auth/token/")
                 .then()
-                .spec(wrongLoginSpec)
+                .log().all()
+                .statusCode(401)
+                .body(matchesJsonSchemaInClasspath("schemas/login/login_unsuccessful_schema.json"))
+                .body("detail", containsString("Invalid username or password."))
                 .extract().as(LoginUnsuccessfulResponseModel.class);
 
         String expectedDetailError = "Invalid username or password.";
